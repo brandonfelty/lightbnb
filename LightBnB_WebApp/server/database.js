0d6c67aa7386c0bb0842573a24d6538aec7448a9
Brandon Felty <brcfelty@gmail.com>
2022-05-05 08:51:50 -0600

    updates get all reservations function to query database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -79,7 +79,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`  SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+    FROM users 
+    JOIN reservations ON users.id = reservations.guest_id
+    JOIN property_reviews ON reservations.id = property_reviews.reservation_id
+    JOIN properties ON properties.id = reservations.property_id
+    WHERE users.id = $1
+    GROUP BY reservations.id, properties.title, properties.cost_per_night, properties.id
+    LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
@@ -95,7 +109,6 @@ exports.getAllReservations = getAllReservations;
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      //console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
